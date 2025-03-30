60135736bcfb895737168b95a644d9cbb2b70f36
marpietro2007 <marpietro2007@gmail.com>
2025-03-30 15:52:55 +0000

    Adaptações de responsividade para o desktop e fim do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export function App() {
   return(
     Submited === false ?(
       <div className="bg-gradient-dark text-white mx-6 p-6
-      rounded-2xl font-overpass">
+      rounded-2xl font-overpass max-w-103">
         <div className="pg-dark-blue w-fit p-4 rounded-full mb-4">
           <img src={iconStar} alt="icon star" />
         </div>
@@ -31,17 +31,17 @@ export function App() {
 
         <p className="text-sm text-light-grey mb-6 leading-1">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
         <div className="flex justify-between mb-6">
-          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleNotaAvaliacao(1)}/>
-          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleNotaAvaliacao(2)}/>
-          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleNotaAvaliacao(3)}/>
-          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleNotaAvaliacao(4)}/>
-          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleNotaAvaliacao(5)}/>
+          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleNotaAvaliacao(1)}/>
+          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleNotaAvaliacao(2)}/>
+          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleNotaAvaliacao(3)}/>
+          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleNotaAvaliacao(4)}/>
+          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover:cursor-pointer" onClick={() => handleNotaAvaliacao(5)}/>
         </div>
 
-        <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3">Submit</button>
+        <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange hover:cursor-pointer">Submit</button>
       </div>
     ) : (
-      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
         <img className="mx-auto mb-6" src={illustrationthankyou} alt="illustration thank you" />
 
         <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
